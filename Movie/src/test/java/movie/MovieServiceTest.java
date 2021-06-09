f27accf8b00cae6175aeb3b502daa84c1110f780
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-06-09 17:22:09 +0200

    Working

---
+++ Movie/src/test/java/movie/MovieServiceTest.java
@@ -0,0 +1,49 @@
+package movie;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieServiceTest {
+    MovieService movieService;
+
+    @BeforeEach
+    void init(){
+        movieService = new MovieService();
+        movieService.save(new Movie("van dame", 120, LocalDate.of(2020,05, 03)));
+        movieService.save(new Movie("Dragula", 100, LocalDate.of(2019,05, 03)));
+        movieService.save(new Movie("True Lies", 160, LocalDate.of(2020,04, 03)));
+
+    }
+    @Test
+    void theNewestFilmTest() {
+        Movie movie = movieService.theNewestFilm();
+
+        assertEquals(movie.getReleaseDate(), LocalDate.of(2020,05, 03));
+    }
+
+    @Test
+    void searchFilmByDateTest() {
+        Movie movie = movieService.searchFilmByDate(LocalDate.of(2020,05, 03));
+
+        assertEquals(movie.getName(), "van dame");
+      }
+
+    @Test
+    void searchFilmTest() {
+        Movie movie = movieService.searchFilm("Dragula");
+
+        assertEquals(movie.getName(), "Dragula");
+    }
+
+    @Test
+    void partSearchFilmTest() {
+        Movie movie = movieService.searchFilm("ragu");
+
+        assertEquals(movie.getName(), "Dragula");
+    }
+}
